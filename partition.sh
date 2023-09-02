ed1f66bfbd99a5747a1b1445d6b2cd58be6231d8
Gurjiwan Singh <gurrjiwan@gmail.com>
2023-09-02 17:46:25 +0530

    custom script for partitioning

---
+++ partition.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+read -p "Please select the device to partition (e.g. /dev/sda): " customvar
+if [[ customvar !> /dev/null ]];then
+    print"Please enter a device \n"
+    exit 1
+fi
+
+read -p "Select a swap size in MiB (default: 0): " swapsizevar
+swapsizevar=${swapsizevar:-0}
+
+sgdisk -Z ${customvar} # ZAP the hardrive to remove any cerated partition
+
+fdisk ${customvar} <<FDISK_Input
+g
+n
+1
+
++512M
+
+t
+1
+
+w
+FDISK_Input
+
+if [[ swapsizevar != 0 ]];then
+    fdisk ${customvar} <<FDISK_swap
+    n
+    2
+
+    +${swapsizevar}M
+
+    t
+    2
+    19
+
+    w
+FDISK_swap
+fi
+
+fdisk ${customvar} <<FDISK_root
+n
+
+
+
+
+w
+FDISK_root
+
+print "Partition script end .. \n"
